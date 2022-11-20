c5b1a7aff43b6a68155ccabe0596e02f9ded8d2b
bloodlustyt <67737806+bloodlustyt@users.noreply.github.com>
2022-11-20 21:40:00 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -13,8 +13,8 @@ API_ID = 3599592
 API_HASH = "80865dfca1e192f81931cbf61203cfe7"
 BOT_TOKEN = "5851808236:AAEc8zGr6Rd1EKbN5x3kAXXICMQepT24mJY"
 FORCESUB = 1485129059
-ACCESS = 630269331
-MONGODB_URI = "mongodb+srv://psychopomp:Cracker@1000@psychopomp.fgbjrip.mongodb.net/?retryWrites=true&w=majority"
+ACCESS = 100630269331
+MONGODB_URI = "mongodb+srv://Caanolisurukar:Caanolisurukar@cluster0.gs9clvq.mongodb.net/?retryWrites=true&w=majority"
 AUTH_USERS = 1058482162
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
